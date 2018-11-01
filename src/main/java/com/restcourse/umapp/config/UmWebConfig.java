6d8769a4fd8a368cccea6b49facc7d4f960e5cd7
abacsrob <abacsrob@gmail.com>
2018-11-01 17:49:34 +0100

    enable the INDENT_OUTPUT and the FAIL_ON_UNKNOWN_PROPERTIES options for the json message converter

--- src/main/java/com/restcourse/umapp/config/UmWebConfig.java
+++ src/main/java/com/restcourse/umapp/config/UmWebConfig.java
@@ -1,12 +1,29 @@
 package com.restcourse.umapp.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @ComponentScan({ "com.restcourse.umapp.web" })
 @EnableWebMvc
 public class UmWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Optional<HttpMessageConverter<?>> converterOptional = converters.stream().filter(converter -> converter instanceof AbstractJackson2HttpMessageConverter).findFirst();
+        if (converterOptional.isPresent()) {
+            final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) converterOptional.get();
+            converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+            converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        }
+    }
 }
