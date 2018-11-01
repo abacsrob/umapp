da9de3d1586a4c0edb2464c5fa685b197227adb5
abacsrob <abacsrob@gmail.com>
2018-11-01 08:52:11 +0100

    Initial commit

---
+++ src/main/java/com/restcourse/umapp/UmappApplication.java
@@ -0,0 +1,12 @@
+package com.restcourse.umapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmappApplication.class, args);
+    }
+}
