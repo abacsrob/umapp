0f06db499ae082ca9d6d6eacf6e27939378d6039
abacsrob <abacsrob@gmail.com>
2018-11-08 18:39:48 +0100

    and the fix is using @NoRepositoryBean

--- src/main/java/com/restcourse/umapp/common/UmRepository.java
+++ src/main/java/com/restcourse/umapp/common/UmRepository.java
@@ -2,6 +2,8 @@ package com.restcourse.umapp.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface UmRepository <T extends UmEntity> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T>, UmSearchableByName<T> {
 }
